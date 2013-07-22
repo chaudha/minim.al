d6713519a2fd13d19e1d22fb282e3a7c5ec9cf8b
Anik Chaudhry <chaudha@miamioh.edu>
2013-07-22 12:30:10 -0400

    Initial Commuit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MinimAl::Application.config.secret_token = '3954cd2e6956b0c009acee5f814945dd597c9377f2d8b574ba69a8ba95c643892dd3d9d5c4f5249fb23cb6d46b011afd0a1a03c638575e3421eca144672f3e4f'
